3a379f09ca45f113b587cff3f029e833f7a366fc
Tyler Gennaro <tylergennaro10@gmail.com>
2023-08-14 14:02:56 -0400

    support different size props

--- src/components/ServiceItem.tsx
+++ src/components/ServiceItem.tsx
@@ -4,14 +4,19 @@ export default function ServiceItem({
 	image,
 	title,
 	href,
+	size = 400,
+	textSize = 'text-2xl',
 }: {
 	image: string;
 	title: string;
 	href?: string;
+	size?: number;
+	textSize?: string;
 }) {
 	return (
 		<a
-			className='w-[400px] aspect-square relative rounded-md overflow-hidden shadow-md [&:hover>div]:h-full [&:hover>img]:scale-110'
+			style={{ width: `${size}px` }}
+			className='aspect-square relative rounded-md overflow-hidden shadow-md [&:hover>div]:h-full [&:hover>img]:scale-110'
 			href={href}
 		>
 			<Image
@@ -21,7 +26,9 @@ export default function ServiceItem({
 				className='object-cover transition-transform duration-500 ease-out'
 			/>
 			<div className='absolute bottom-0 left-0 right-0 h-16 bg-black/60 text-center flex items-center justify-center transition-all ease-out duration-500'>
-				<span className='text-2xl text-white font-semibold uppercase tracking-wide'>
+				<span
+					className={`${textSize} text-white font-semibold uppercase tracking-wide`}
+				>
 					{title}
 				</span>
 			</div>
