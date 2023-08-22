d0528f33f3324e814538c5283a86e6c139b45c1e
Tyler Gennaro <tylergennaro10@gmail.com>
2023-08-21 21:53:56 -0400

    began work on showcase

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,37 @@
+import HeaderImage from '@/components/HeaderImage';
+import Section from '@/components/Section';
+import Image from 'next/image';
+
 export default function Page() {
-	return <div>Whoops! Nothing to see here.</div>;
+	return (
+		<main>
+			<HeaderImage
+				image='/projects/cit_bank_park.jpg'
+				alt='citizens bank park'
+				title='Project Showcase'
+				imageClass='object-bottom'
+			/>
+			<Section>
+				<div>
+					<h1 className='text-4xl font-semibold mb-2'>SEPTA Wawa Station</h1>
+					<h2 className='text-sm'>1490 West Baltimore Pike, Media, PA 19063</h2>
+					<h3 className='py-8 text-xl font-medium'>
+						Fabricated and installed the railings.
+					</h3>
+					<div className='flex gap-4 flex-wrap'>
+						{Array.from({ length: 8 }).map((_, i) => (
+							<div className='relative w-64 h-80'>
+								<Image
+									src={`/projects/wawa_station/wawa-${i + 1}.jpg`}
+									alt={`wawa_station_${i}`}
+									fill={true}
+									className='object-cover'
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+			</Section>
+		</main>
+	);
 }
