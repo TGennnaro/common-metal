e7ca1329113c04bc1439475e574e4ccd887bb644
Tyler Gennaro <tylergennaro10@gmail.com>
2023-08-24 12:16:11 -0400

    added project gallery to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,5 @@
 import HeaderImage from '@/components/HeaderImage';
-import Section from '@/components/Section';
-import Image from 'next/image';
+import ImageGallery from './ImageGallery';
 
 export default function Page() {
 	return (
@@ -11,27 +10,65 @@ export default function Page() {
 				title='Project Showcase'
 				imageClass='object-bottom'
 			/>
-			<Section>
-				<div>
-					<h1 className='text-4xl font-semibold mb-2'>SEPTA Wawa Station</h1>
-					<h2 className='text-sm'>1490 West Baltimore Pike, Media, PA 19063</h2>
-					<h3 className='py-8 text-xl font-medium'>
-						Fabricated and installed the railings.
-					</h3>
-					<div className='flex gap-4 flex-wrap'>
-						{Array.from({ length: 8 }).map((_, i) => (
-							<div className='relative w-64 h-80' key={i}>
-								<Image
-									src={`/projects/wawa_station/wawa-${i + 1}.jpg`}
-									alt={`wawa_station_${i}`}
-									fill={true}
-									className='object-cover'
-								/>
-							</div>
-						))}
-					</div>
+			<ImageGallery />
+			{/* <Section className='gap-4'>
+				<ProjectHeader
+					title='SEPTA Wawa Station'
+					service='railing installation'
+					location='1490 West Baltimore Pike, Media, PA 19063'
+				/>
+				<div className='flex gap-4 flex-wrap'>
+					{Array.from({ length: 9 }).map((_, i) => (
+						<div className='relative w-64 h-80' key={i}>
+							<Image
+								src={`/projects/wawa_station/wawa-${i + 1}.jpg`}
+								alt={`wawa_station_${i}`}
+								fill={true}
+								className='object-cover rounded-md shadow-md'
+							/>
+						</div>
+					))}
 				</div>
 			</Section>
+			<Divider />
+			<Section className='gap-4'>
+				<ProjectHeader
+					title='SEPTA Secane Station'
+					service='railing installation'
+					location='1297 Providence Road, Clifton Heights, PA 19018'
+				/>
+				<div className='flex gap-4 flex-wrap'>
+					{Array.from({ length: 8 }).map((_, i) => (
+						<div className='relative w-64 h-80' key={i}>
+							<Image
+								src={`/projects/secane_station/secane-${i + 1}.jpg`}
+								alt={`secane_station_${i}`}
+								fill={true}
+								className='object-cover rounded-md shadow-md'
+							/>
+						</div>
+					))}
+				</div>
+			</Section>
+			<Section className='gap-4'>
+				<ProjectHeader
+					title='Unknown Railings'
+					service='railing fabrication'
+					location='Unknown location'
+				/>
+				<div className='flex gap-4 flex-wrap'>
+					{Array.from({ length: 9 }).map((_, i) => (
+						<div className='relative w-64 h-80' key={i}>
+							<Image
+								src={`/projects/unknown_railings/unknown-${i + 1}.jpg`}
+								alt={`unknown_railings_${i}`}
+								fill={true}
+								className='object-cover rounded-md shadow-md'
+							/>
+						</div>
+					))}
+				</div>
+			</Section> */}
 		</main>
 	);
 }
