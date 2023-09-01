6d7d6f1f40737ef822c7f984016f94cda16c352e
Tyler Gennaro <tylergennaro10@gmail.com>
2023-09-01 09:16:44 -0400

    improved cn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import config from '@/config/config';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...args: string[]) {
+export function cn(...args: (string | undefined)[]) {
 	return twMerge(...args);
 }
 
