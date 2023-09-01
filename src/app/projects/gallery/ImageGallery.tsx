d9ef6e58d4229e922b77474e5569b57961d919ab
Tyler Gennaro <tylergennaro10@gmail.com>
2023-09-01 09:15:54 -0400

    new general-purpose gallery

---
+++ src/app/projects/gallery/ImageGallery.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/captions.css';
+
+import Section from '@/components/Section';
+import Image from 'next/image';
+import { useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+import Captions from 'yet-another-react-lightbox/plugins/captions';
+
+const gallery = [
+	{
+		label: 'Galvanized Railings',
+		path: '/projects/galvanized_railings/gal-%d.jpg',
+		length: 4,
+	},
+	{
+		label: 'Architectural Railroad Shelters',
+		path: '/projects/architectural_rail_shelters/arch-%d.jpg',
+		length: 16,
+	},
+	{
+		label: 'Stainless Steel Elevators',
+		path: '/projects/elevators/elevator-%d.jpg',
+		length: 5,
+	},
+	{
+		label: 'Stainless Steel Staircases',
+		path: '/projects/stairs/stairs-%d.jpg',
+		length: 10,
+	},
+	{
+		label: 'Miscellaneous Projects',
+		path: '/projects/misc/misc-%d.jpg',
+		length: 34,
+	},
+];
+
+export default function ImageGallery() {
+	const [index, setIndex] = useState(-1);
+	const [page, setPage] = useState(-1);
+	return (
+		<>
+			{gallery.map(({ label, path, length }, sectionIndex) => (
+				<Section key={sectionIndex} className='py-8'>
+					<div className='sticky top-20 bg-white py-4 z-[1] after:-z-[1] after:absolute after:left-0 after:right-0 after:border-t after:border-zinc-300 after:top-1/2 after:-translate-y-1/2'>
+						<span className='bg-white pr-4 font-semibold leading-6 text-xl'>
+							{label}
+						</span>
+					</div>
+					<div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-4'>
+						{Array.from({ length }).map((_, i) => (
+							<div
+								className='relative rounded-md aspect-[3/4] [&:hover>div]:opacity-100 [&:hover>img]:scale-110 overflow-hidden'
+								key={i}
+							>
+								<Image
+									src={path.replace('%d', `${i + 1}`)}
+									alt={`${label}_${i}`}
+									fill={true}
+									className='object-cover shadow-md cursor-pointer transition-transform duration-300'
+									onClick={() => {
+										setIndex(i);
+										setPage(sectionIndex);
+									}}
+								/>
+								<div className='absolute inset-0 bg-black/30 pointer-events-none opacity-0 transition-opacity duration-300' />
+							</div>
+						))}
+					</div>
+					<Lightbox
+						plugins={[Captions]}
+						slides={Array.from({ length: length }).map((_, i) => ({
+							src: path.replace('%d', `${i + 1}`),
+							alt: `${label}_${i}`,
+							title: `${label} (${i + 1}/${length})`,
+						}))}
+						index={index}
+						open={index > -1 && page === sectionIndex}
+						close={() => {
+							setIndex(-1);
+							setPage(-1);
+						}}
+					/>
+				</Section>
+			))}
+		</>
+	);
+}
--- src/app/projects/gallery/page.tsx
+++ src/app/projects/gallery/page.tsx
@@ -1,28 +1,21 @@
 import Section from '@/components/Section';
 import { getMetadata } from '@/lib/utils';
 import { Metadata } from 'next';
+import ImageGallery from './ImageGallery';
+import HeaderImage from '@/components/HeaderImage';
 
 export const metadata: Metadata = getMetadata('Gallery');
 
 export default function Page() {
 	return (
 		<main>
-			<Section>
-				<div className='sticky top-20 bg-white py-4 z-[1] after:-z-[1] after:absolute after:left-0 after:right-0 after:border-t after:border-zinc-300 after:top-1/2 after:-translate-y-1/2'>
-					<span className='bg-white pr-4 font-semibold leading-6'>
-						Galvanized Railings
-					</span>
-				</div>
-				<div className='bg-zinc-400 w-96 h-[500px]' />
-				<div className='bg-zinc-400 w-96 h-[500px]' />
-				<div className='bg-zinc-400 w-96 h-[500px]' />
-			</Section>
-			<Section>
-				<h1 className='sticky top-20'>Grates</h1>
-				<div className='bg-zinc-500 w-96 h-[500px]' />
-				<div className='bg-zinc-500 w-96 h-[500px]' />
-				<div className='bg-zinc-500 w-96 h-[500px]' />
-			</Section>
+			<HeaderImage
+				image='/projects/gallery_header.jpg'
+				alt='gallery header'
+				title='Project Gallery'
+				imageClass='object-center'
+			/>
+			<ImageGallery />
 		</main>
 	);
 }
