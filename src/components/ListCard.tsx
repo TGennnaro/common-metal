5dfaf461b5c0eaac921697629ed6213b569bb52c
Tyler Gennaro <tylergennaro10@gmail.com>
2023-09-13 15:53:53 -0400

    fixed responsiveness

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -8,13 +8,13 @@ export default function ListCard({
 	title: string;
 }) {
 	return (
-		<div className='w-2/3 h-16 relative rounded-md overflow-hidden flex items-center p-8 [&:hover_img]:filter-none [&:hover_img]:scale-110 [&:hover>div::after]:-translate-x-1/2 shadow-md hover:shadow-xl hover:scale-[1.02] transition-all'>
-			<div className='absolute inset-0 after:absolute after:inset-0 after:bg-gradient-to-r after:from-black/40 after:to-transparent after:from-80% after:transition-transform after:duration-300 after:ease-out pointer-events-none'>
+		<div className='w-full max-w-2xl h-16 relative rounded-md overflow-hidden flex items-center p-8 [&:hover_img]:filter-none [&:hover_img]:scale-110 lg:[&:hover>div::after]:-translate-x-1/2 shadow-md hover:shadow-xl hover:scale-[1.02] transition-all'>
+			<div className='absolute inset-0 after:absolute after:inset-0 after:bg-gradient-to-r after:from-black/40 after:to-transparent after:from-80% after:transition-transform after:duration-300 after:ease-out pointer-events-none after:-translate-x-1/2 lg:after:translate-x-0'>
 				<Image
 					src={image}
 					alt={title}
 					fill={true}
-					className='object-cover object-center grayscale [transition:_400ms_filter_ease-out,_200ms_transform_ease-out]'
+					className='object-cover object-center lg:grayscale [transition:_400ms_filter_ease-out,_200ms_transform_ease-out]'
 				/>
 			</div>
 			<span className='z-10 text-2xl text-white font-semibold uppercase tracking-wide'>
