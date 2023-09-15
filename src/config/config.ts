51f9f1a469be3630b1c178106adcd861d9dc38bf
Tyler Gennaro <tylergennaro10@gmail.com>
2023-09-15 12:53:41 -0400

    updated default description

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ const config = {
 	seo: {
 		title: '%s | Commonwealth Metal Company',
 		description:
-			"Looking for top-notch steel and metal fabrication and welding services in Huntingdon Valley, PA? Commonwealth Metal Company has you covered. With unmatched expertise in miscellaneous steel projects, our skilled team crafts precision-engineered solutions tailored to your needs. Whether it's custom fabrication, welding, or general metalwork, we deliver superior quality and exceptional craftsmanship. Discover the difference of working with Commonwealth Metal Company today.",
+			'Are you in search of exceptional steel and stainless steel fabrication and welding services in the heart of Huntingdon Valley, PA? Look no further than Commonwealth Metal Company! Our team is adept at handling a wide array of architectural steel projects, including railings, stairs, screens, benches, windscreens, and more. With a commitment to precision engineering and craftsmanship, we specialize in custom fabrication, welding, and general metalwork. Whether you require intricate architectural railings, sleek stainless steel screens, or sturdy benches with tangent pales, we have the expertise to bring your vision to life. Experience the Commonwealth Metal Company difference today, where top-notch fabrication and welding services meet your unique needs.',
 	},
 	maxEmailCharacters: 1000,
 };
